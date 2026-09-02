d8d5a9e78e3e820922f7c759895aaef3d14ac1f3
fatherlei <cecyleiva@gmail.com>
2026-09-02 01:04:48 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'not-app-v5';
+const CACHE = 'not-app-v18';
 const ASSETS = ['./index.html', './manifest.json', './logo.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
